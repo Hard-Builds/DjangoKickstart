c7b5b552d5d7cd3c837a7684d3b5e82d0d8026cb
HardU99 <hardykshukla@gmail.com>
2025-03-02 18:33:04 +0530

    Added profile table related to the user

--- django_kickstart/settings.py
+++ django_kickstart/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.conf.global_settings import LOGIN_REDIRECT_URL
@@ -134,3 +134,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = "login"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- django_kickstart/urls.py
+++ django_kickstart/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
@@ -15,5 +17,11 @@ urlpatterns = [
         "logout/",
         auth_views.LogoutView.as_view(template_name="users/logout.html"),
         name="logout"
-    )
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        prefix=settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
