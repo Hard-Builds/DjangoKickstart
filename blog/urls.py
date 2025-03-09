0d17e5ffc927ce5426f6dc73ff0fd0890a3a8b9f
HardU99 <hardykshukla@gmail.com>
2025-03-09 08:24:21 +0530

    Added pagination and user wise post listing

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from blog.views import about_view, PostListView, DetailedPostView, AddPostView, \
-    UpdatePostView, DeletePostView
+    UpdatePostView, DeletePostView, UserPostListView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="blog-home"),
+    path("user/<str:username>", UserPostListView.as_view(), name="user-posts"),
     path("post/<int:pk>/", DetailedPostView.as_view(), name="blog-details"),
     path("post/new/", AddPostView.as_view(), name="blog-create"),
     path("post/<int:pk>/update/", UpdatePostView.as_view(),
