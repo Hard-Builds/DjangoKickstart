c4dfefe184df625c13bd8335e8feb8f04df26ddb
Hardik-SmartSense <hardik.shukla@smartsensesolutions.com>
2025-03-31 16:00:32 +0530

    Added blog home as default page

--- django_kickstart/urls.py
+++ django_kickstart/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="blog/")),
     path("blog/", include("blog.urls")),
     path("users/", include("users.urls")),
     path(
