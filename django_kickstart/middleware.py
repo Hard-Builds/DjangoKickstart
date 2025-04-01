4106c59c3cc2f80ba73a46da1855dee35faf0a5c
Hardik-SmartSense <hardik.shukla@smartsensesolutions.com>
2025-04-01 16:33:06 +0530

    Added logger

--- django_kickstart/middleware.py
+++ django_kickstart/middleware.py
@@ -1,16 +1,25 @@
+import logging
 import time
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class PerformanceMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, request):
+
         start_time = time.time()
-        response = self.get_response(*args, **kwargs)
+        response = self.get_response(request)
         time_taken = time.time() - start_time
         time_taken = round(time_taken, 4)
 
-        response.headers["time_taken"] = time_taken
+        response.headers["time-taken"] = time_taken
 
+        logger.info(
+            f"API Path: {request.path_info}, method:"
+            f" {request.method}, user: {request.user.pk}, time_taken: {time_taken}"
+        )
         return response
