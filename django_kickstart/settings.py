ad7d1968d57f6f72914606792c0a433472e4a037
Hardik-SmartSense <hardik.shukla@smartsensesolutions.com>
2025-04-01 11:54:17 +0530

    Added API performance tracker middleware to append time taken by API in the response headers

--- django_kickstart/settings.py
+++ django_kickstart/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    "django_kickstart.middleware.PerformanceMiddleware",
 ]
 
 ROOT_URLCONF = "django_kickstart.urls"
